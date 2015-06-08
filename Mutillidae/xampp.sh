b4081402bbf8acf6b0147c78be0de7ee4c70434d
andylongshaw <andy@blueskyline.com>
2015-06-08 22:29:29 +0100

    x64 ubuntu script with XAMPP

---
+++ Mutillidae/xampp.sh
@@ -0,0 +1,15 @@
+echo "Provisioning XAMPP"
+
+cd /vagrant
+
+chmod 755 xampp-linux-x64-*-installer.run
+
+sudo ./xampp-linux-x64-*-installer.run
+
+ed /opt/lampp/etc/extra/httpd-xampp.conf << EOF
+/Require local/s/^/#/
+wq
+EOF
+
+sudo /opt/lampp/lampp restart
+
