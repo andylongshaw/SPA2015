a61e3e983f30779035fefb6da8a5f51fac06746a
andylongshaw <andy@blueskyline.com>
2015-06-25 13:16:25 +0100

    fixed motd IP address bug

--- Mutillidae/config.sh
+++ Mutillidae/config.sh
@@ -9,14 +9,16 @@ SPA2015x
 SPA2015x
 EOF
 
-sudo ed /etc/motd.tail << EOF >> $INSTALL_LOG 2>&1
+sudo ed /etc/profile << EOF >> $INSTALL_LOG 2>&1
 $
 a
 
-Your IP address is `ifconfig eth1 | grep 'inet addr:' | sed 's/ *//' | cut -d ' ' -f2 | cut -d ':' -f2`
+echo Your IP address is `ifconfig eth1 | grep 'inet addr:' | sed 's/ *//' | cut -d ' ' -f2 | cut -d ':' -f2`
+
+set -o vi
 .
 wq
 EOF
-if [ $? -ne 0 ]; then echo "edit of motd file failed"; fi
+if [ $? -ne 0 ]; then echo "edit of profile file failed"; fi
 
 sudo umount /vagrant >> $INSTALL_LOG 2>&1 || echo "unmount vagrant synced failed"
